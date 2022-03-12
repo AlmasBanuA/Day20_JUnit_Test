63ace00c7184191284aeab5ec4b68e2d5e015131
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-03-12 16:28:35 +0530

    Valid PhoneNumber Using Test Cases

--- src/test/java/com/junit/UserRegistrationTest.java
+++ src/test/java/com/junit/UserRegistrationTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * testCases for validating EmailId
+ * testCases for validating PhoneNumber
  *
  */
 public class UserRegistrationTest {
@@ -57,28 +57,51 @@ public class UserRegistrationTest {
 		boolean isLnameINValid = userRegistration.Lname("advani");
 		Assert.assertFalse(isLnameINValid);
 	}
-	
+
 	@Test
 	/**
 	 * created method testEmailId_MustReturnTrue() for true condition
 	 */
-    public void testEmailId_MustReturnTrue() {
-        UserRegistration userRegistration = new UserRegistration();
-        /**
+	public void testEmailId_MustReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		/**
 		 * calling checkEmail method to see mailId is valid
 		 */
-        boolean isEmailValid = userRegistration.checkEmail("almas.ab@gmail.com");
-        Assert.assertTrue(isEmailValid);
-    }
+		boolean isEmailValid = userRegistration.checkEmail("almas.ab@gmail.com");
+		Assert.assertTrue(isEmailValid);
+	}
 
-    @Test
-    /**
+	@Test
+	/**
 	 * created method testEmailId_MustReturnFalse() for false condition
 	 */
 
-    public void testEmailId_MustReturnFalse() {
-        UserRegistration userRegistration = new UserRegistration();
-        boolean isEmailInvalid = userRegistration.checkEmail("almas@.com");
-        Assert.assertFalse(isEmailInvalid);
-    }
+	public void testEmailId_MustReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isEmailInvalid = userRegistration.checkEmail("almas@.com");
+		Assert.assertFalse(isEmailInvalid);
+	}
+
+	@Test
+	/**
+	 * created testPhoneNumber_MustReturnTrue() for True condition
+	 */
+	public void testPhoneNumber_MustReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		/**
+		 * calling checkPhoneNum method to see phoneNumber is valid
+		 */
+		boolean isPhoneNumberValid = userRegistration.checkPhoneNum("91 7903990740");
+		Assert.assertTrue(isPhoneNumberValid);
+	}
+
+	@Test
+	/**
+	 * created testPhoneNumber_MustReturnFalse() for False condition
+	 */
+	public void testPhoneNumber_MustReturnFalse() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean isPhoneNumberInValid = userRegistration.checkPhoneNum("7903990740");
+		Assert.assertFalse(isPhoneNumberInValid);
+	}
 }
